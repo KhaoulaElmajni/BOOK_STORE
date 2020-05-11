c036cc907e22a1466a2c375887197d23745a3662
KhaoulaElmajni <elmajnikhaoula99@gmail.com>
2020-05-11 16:49:30 +0000

    search by category and 404 error page

---
+++ book-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-book-list',
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
+
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[];
+  currentCategoryId : number;
+
+  constructor(private _bookService: BookService,
+          private _activatedRoute: ActivatedRoute ) { }
+
+  ngOnInit(){
+    this._activatedRoute.paramMap.subscribe(()=>{
+      this.listBooks();
+    })
+  }
+
+
+  listBooks(){
+
+    const hasCategoryId : boolean = this._activatedRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      this.currentCategoryId= +this._activatedRoute.snapshot.paramMap.get('id');
+
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
+      data =>this.books = data
+    )
+  }
+
+}
